9c2f8ca8ec8f37193e418fd5392f5d33dd5ffdd3
parkmuhyeun <pjhg410@gmail.com>
2022-01-04 22:53:19 +0900

    Resend confirm email

--- src/main/java/com/studyship/account/AccountController.java
+++ src/main/java/com/studyship/account/AccountController.java
@@ -68,5 +68,23 @@ public class AccountController {
         return view;
     }
 
+    @GetMapping("/check-email")
+    String checkEmail(@CurrentUser Account account, Model model) {
+        model.addAttribute("email", account.getEmail());
+        return "account/check-email";
+    }
+
+    @GetMapping("/resend-confirm-email")
+    String resendConfirmEmail(@CurrentUser Account account, Model model) {
+        if (!account.canSendConfirmEmail()) {
+            model.addAttribute("error", "인증 이메일은 1시간에 한번만 전송할 수 있습니다.");
+            model.addAttribute("email", account.getEmail());
+            return "account/check-email";
+        }
+
+        accountService.sendSignUpConfirmEmail(account);
+        return "redirect:/";
+    }
+
 }
 
