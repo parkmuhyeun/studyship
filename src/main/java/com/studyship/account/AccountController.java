cb7b424fe1bda07d9925e8f87586f46ef5a905e0
parkmuhyeun <pjhg410@gmail.com>
2022-01-05 23:31:22 +0900

    Update Notifications

--- src/main/java/com/studyship/account/AccountController.java
+++ src/main/java/com/studyship/account/AccountController.java
@@ -96,7 +96,5 @@ public class AccountController {
         model.addAttribute("isOwner", byNickname.equals(account));
         return "account/profile";
     }
-
-
 }
 
--- src/main/java/com/studyship/account/AccountService.java
+++ src/main/java/com/studyship/account/AccountService.java
@@ -1,6 +1,7 @@
 package com.studyship.account;
 
 import com.studyship.domain.Account;
+import com.studyship.settings.Notifications;
 import com.studyship.settings.Profile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
@@ -93,6 +94,16 @@ public class AccountService implements UserDetailsService {
 
     public void updatePassword(Account account, String newPassword) {
         account.setPassword(passwordEncoder.encode(newPassword));
+        accountRepository.save(account);
+    }
+
+    public void updateNotifications(Account account, Notifications notifications) {
+        account.setStudyCreatedByWeb(notifications.isStudyCreatedByWeb());
+        account.setStudyCreatedByEmail(notifications.isStudyCreatedByEmail());
+        account.setStudyUpdatedByWeb(notifications.isStudyUpdatedByWeb());
+        account.setStudyUpdatedByEmail(notifications.isStudyUpdatedByEmail());
+        account.setStudyEnrollmentResultByEmail(notifications.isStudyEnrollmentResultByEmail());
+        account.setStudyEnrollmentResultByWeb(notifications.isStudyEnrollmentResultByWeb());
         accountRepository.save(account);
     }
 }
