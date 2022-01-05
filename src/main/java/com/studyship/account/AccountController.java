48c2b131ef25008e5fa194de9a0a28141ecde24f
parkmuhyeun <pjhg410@gmail.com>
2022-01-05 14:49:11 +0900

    Profile view

--- src/main/java/com/studyship/account/AccountController.java
+++ src/main/java/com/studyship/account/AccountController.java
@@ -86,5 +86,16 @@ public class AccountController {
         return "redirect:/";
     }
 
+    @GetMapping("/profile/{nickname}")
+    public String viewProfile(@PathVariable String nickname, Model model, @CurrentUser Account account) {
+        Account byNickname = accountRepository.findByNickname(nickname);
+        if (nickname == null) {
+            throw new IllegalArgumentException(nickname + "에 해당하는 사용자가 없습니다.");
+        }
+
+        model.addAttribute(byNickname);
+        model.addAttribute("isOwner", byNickname.equals(account));
+        return "account/profile";
+    }
 }
 
