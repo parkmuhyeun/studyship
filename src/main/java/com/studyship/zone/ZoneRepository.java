ff88030cd35a9e2137332fea69c716dc6f88cb85
parkmuhyeun <pjhg410@gmail.com>
2022-01-06 17:56:33 +0900

    Zone domain and init data

---
+++ src/main/java/com/studyship/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.studyship.zone;
+
+import com.studyship.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
