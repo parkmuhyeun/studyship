4adde74edd5529a5187c8e7b11d1f94509c893c7
parkmuhyeun <pjhg410@gmail.com>
2022-01-08 23:03:12 +0900

    Added tests for creating and querying study

---
+++ src/test/java/com/studyship/study/StudyControllerTest.java
@@ -0,0 +1,107 @@
+package com.studyship.study;
+
+import com.studyship.WithAccount;
+import com.studyship.account.AccountRepository;
+import com.studyship.domain.Account;
+import com.studyship.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
+class StudyControllerTest {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired StudyRepository studyRepository;
+    @Autowired AccountRepository accountRepository;
+    @Autowired StudyService studyService;
+
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
+    }
+
+    @Test
+    @WithAccount("muto")
+    @DisplayName("스터디 개설 폼 조회")
+    void createStudyForm() throws Exception {
+        mockMvc.perform(get("/new-study"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("studyForm"))
+                .andExpect(view().name("study/form"));
+    }
+
+    @Test
+    @WithAccount("muto")
+    @DisplayName("스터디 개설 - 완료")
+    void createStudy_success() throws Exception {
+        mockMvc.perform(post("/new-study")
+                .param("path", "test-path")
+                .param("title", "study title")
+                .param("shortDescription", "short description of a study")
+                .param("fullDescription", "full description of a study")
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/test-path"));
+
+        Study study = studyRepository.findByPath("test-path");
+        assertNotNull(study);
+        Account account = accountRepository.findByNickname("muto");
+        assertTrue(study.getManagers().contains(account));
+    }
+
+    @Test
+    @WithAccount("muto")
+    @DisplayName("스터디 개설 - 실패")
+    void createStudy_fail() throws Exception {
+        mockMvc.perform(post("/new-study")
+                .param("path", "wrong path")
+                .param("title", "study title")
+                .param("shortDescription", "short description of a study")
+                .param("fullDescription", "full description of a study")
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("study/form"))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("studyForm"));
+
+        Study study = studyRepository.findByPath("wrong path");
+        assertNull(study);
+    }
+
+    @Test
+    @WithAccount("muto")
+    @DisplayName("스터디 조회")
+    void viewStudy() throws Exception {
+        Study study = new Study();
+        study.setPath("test-path");
+        study.setTitle("test study");
+        study.setShortDescription("short description");
+        study.setFullDescription("<p>full description</p>");
+
+        Account muto = accountRepository.findByNickname("muto");
+        studyService.createNewStudy(study, muto);
+
+        mockMvc.perform(get("/study/test-path"))
+                .andExpect(view().name("study/view"))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("study"));
+    }
+
+}
