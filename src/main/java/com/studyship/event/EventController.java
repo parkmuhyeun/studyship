d0f1966e4dcc57e2e5c379bb4475db9eee491795
parkmuhyeun <pjhg410@gmail.com>
2022-01-10 11:36:02 +0900

    Event Create

---
+++ src/main/java/com/studyship/event/EventController.java
@@ -0,0 +1,59 @@
+package com.studyship.event;
+
+import com.studyship.account.CurrentAccount;
+import com.studyship.domain.Account;
+import com.studyship.domain.Event;
+import com.studyship.domain.Study;
+import com.studyship.event.form.EventForm;
+import com.studyship.event.validator.EventValidator;
+import com.studyship.study.StudyService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/study/{path}")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final StudyService studyService;
+    private final EventService eventService;
+    private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
+
+    @InitBinder("eventForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(eventValidator);
+
+    }
+
+    @GetMapping("/new-event")
+    public String newEventForm(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+        model.addAttribute(study);
+        model.addAttribute(account);
+        model.addAttribute(new EventForm());
+        return "event/form";
+    }
+
+    @PostMapping("/new-event")
+    public String newEventSubmit(@CurrentAccount Account account, @PathVariable String path,
+                                 @Valid EventForm eventForm, Errors errors, Model model) {
+        Study study = studyService.getStudyToUpdateStatus(account, path);
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            model.addAttribute(study);
+            return "event/form";
+        }
+
+        Event event = eventService.createEvent(modelMapper.map(eventForm, Event.class), study, account);
+        return "redirect:/study/" + study.getPath() + "/events/" + event.getId();
+    }
+
+}
