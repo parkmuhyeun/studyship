c8c9170edddb0c0d0919c062452c26e13012b46e
parkmuhyeun <pjhg410@gmail.com>
2022-01-11 18:26:36 +0900

    Notification List

--- src/main/java/com/studyship/modules/account/AccountPredicates.java
+++ src/main/java/com/studyship/modules/account/AccountPredicates.java
@@ -10,6 +10,6 @@ public class AccountPredicates {
 
     public static Predicate findByTagsAndZones(Set<Tag> tags, Set<Zone> zones) {
         QAccount account = QAccount.account;
-        return account.zones.any().in(zones).and(account.tags.any().in(tags));
+        return account.zones.any().in(zones).or(account.tags.any().in(tags));
     }
 }
