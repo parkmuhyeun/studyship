421796139f66e4cbc28f9126e6c34a18b872c1b4
parkmuhyeun <pjhg410@gmail.com>
2022-01-12 10:22:44 +0900

    Search Study by keyword

--- src/main/java/com/studyship/modules/main/MainController.java
+++ src/main/java/com/studyship/modules/main/MainController.java
@@ -3,13 +3,20 @@ package com.studyship.modules.main;
 import com.studyship.modules.account.Account;
 import com.studyship.modules.account.CurrentAccount;
 import com.studyship.modules.notification.NotificationRepository;
+import com.studyship.modules.study.Study;
+import com.studyship.modules.study.StudyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final StudyRepository studyRepository;
 
     @GetMapping("/")
     String home(@CurrentAccount Account account, Model model) {
@@ -23,5 +30,13 @@ public class MainController {
     @GetMapping("/login")
     String login() {
         return "login";
+    }
+
+    @GetMapping("/search/study")
+    String search(String keyword, Model model) {
+        List<Study> studyList =  studyRepository.findByKeyword(keyword);
+        model.addAttribute(studyList);
+        model.addAttribute("keyword", keyword);
+        return "search";
     }
 }
