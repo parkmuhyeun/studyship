f607ca17c55717a74b091049c7ffb12eec88ef50
parkmuhyeun <pjhg410@gmail.com>
2022-01-27 22:15:27 +0900

    Home Page After Login

--- src/main/java/com/studyship/modules/study/StudyRepository.java
+++ src/main/java/com/studyship/modules/study/StudyRepository.java
@@ -1,13 +1,18 @@
 package com.studyship.modules.study;
 
+import com.studyship.modules.account.Account;
+import com.studyship.modules.tag.Tag;
+import com.studyship.modules.zone.Zone;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Transactional(readOnly = true)
-public interface StudyRepository extends JpaRepository<Study, Long> ,StudyRepositoryExtension{
+public interface StudyRepository extends JpaRepository<Study, Long> ,StudyRepositoryExtension {
     boolean existsByPath(String path);
 
     @EntityGraph(attributePaths = {"tags", "zones", "managers", "members"}, type = EntityGraph.EntityGraphType.LOAD)
@@ -33,5 +38,10 @@ public interface StudyRepository extends JpaRepository<Study, Long> ,StudyReposi
     @EntityGraph(attributePaths = {"members", "managers"})
     Study findStudyWithManagersAndMembersById(Long id);
 
+    @EntityGraph(attributePaths = {"zones", "tags"})
     List<Study> findFirst9ByPublishedAndClosedOrderByPublishedDateTimeDesc(boolean published, boolean closed);
+
+    List<Study> findFirst5ByManagersContainingAndClosedOrderByPublishedDateTimeDesc(Account account, boolean closed);
+
+    List<Study> findFirst5ByMembersContainingAndClosedOrderByPublishedDateTimeDesc(Account account, boolean closed);
 }
